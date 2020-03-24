6d573b9034507280d92ecd23208cd0baab6cc7c1
Warren R. Bank <warren.r.bank@gmail.com>
2020-03-23 17:48:07 -0700

    improve intent-filter support for absolute directory paths

--- android-studio-project/ExoPlayer-AirPlay-Receiver/src/main/java/com/github/warren_bank/exoplayer_airplay_receiver/ui/StartNetworkingServiceActivity.java
+++ android-studio-project/ExoPlayer-AirPlay-Receiver/src/main/java/com/github/warren_bank/exoplayer_airplay_receiver/ui/StartNetworkingServiceActivity.java
@@ -10,6 +10,8 @@ import com.github.warren_bank.exoplayer_airplay_receiver.MainApp;
 import com.github.warren_bank.exoplayer_airplay_receiver.service.NetworkingService;
 import com.github.warren_bank.exoplayer_airplay_receiver.utils.NetworkUtils;
 
+import java.io.File;
+
 public class StartNetworkingServiceActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +35,79 @@ public class StartNetworkingServiceActivity extends Activity {
   }
 
   private void forwardMedia(Intent intent) {
-    Uri data = getIntent().getData();
+    String action = getIntent().getAction();
+    Uri data = null;
+    String type;
+
+    if (action == null) return;
+
+    try {
+      switch(action) {
+        case Intent.ACTION_VIEW : {
+          data = getIntent().getData();
+          type = getIntent().getType();
+
+          if (type != null) {
+            type = type.toLowerCase();
+
+            switch(type) {
+              case "application/octet-stream" :
+              case "resource/folder" : {
+                data = filterDirectory(data);
+                break;
+              }
+            }
+          }
+          break;
+        }
+        case Intent.ACTION_SEND : {
+          data = (Uri) getIntent().getParcelableExtra(Intent.EXTRA_STREAM);
+          data = filterDirectory(data);
+          break;
+        }
+      }
+    }
+    catch(Exception e) {
+      data = null;
+    }
 
     if (data != null) {
       intent.setAction(NetworkingService.ACTION_PLAY);
       intent.setData(data);
     }
+  }
+
+  private Uri filterDirectory(Uri data) {
+    Uri filtered = data;
+    String scheme, path, uri;
+    File file;
+
+    try {
+      if (data == null)
+        throw new Exception();
+
+      scheme = data.getScheme();
+      if (scheme == null)
+        throw new Exception();
+      if (!scheme.toLowerCase().equals("file"))
+        throw new Exception();
+
+      path = data.getPath();
+      file = new File(path);
+      if (!file.isDirectory())
+        throw new Exception();
+
+      // normalize uri, such that directory path always ends with '/' separator
+      uri = data.toString();
+      if (uri.charAt(uri.length() - 1) != '/') {
+        uri += "/";
+        filtered = Uri.parse(uri);
+      }
+    }
+    catch (Exception e) {
+      filtered = null;
+    }
+
+    return filtered;
   }
 }
