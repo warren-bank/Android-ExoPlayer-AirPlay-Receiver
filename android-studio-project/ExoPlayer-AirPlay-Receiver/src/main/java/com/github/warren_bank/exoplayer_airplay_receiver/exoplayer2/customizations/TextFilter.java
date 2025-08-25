850edafde5494d1302884f11f2f0aeded7a836ce
helpgod11111 <helpgod11111@gmail.com>
2025-08-25 11:00:50 +0800

    Delete android-studio-project directory

--- android-studio-project/ExoPlayer-AirPlay-Receiver/src/main/java/com/github/warren_bank/exoplayer_airplay_receiver/exoplayer2/customizations/TextFilter.java
+++
@@ -1,6 +0,0 @@
-package com.github.warren_bank.exoplayer_airplay_receiver.exoplayer2.customizations;
-
-public interface TextFilter {
-  void setTextFilters(String[] textFilters);
-  void addTextFilters(String[] textFilters);
-}
--- android-studio-project/ExoPlayer-AirPlay-Receiver/src/main/java/com/github/warren_bank/exoplayer_airplay_receiver/exoplayer2/customizations/TextSynchronizer.java
+++
@@ -1,7 +0,0 @@
-package com.github.warren_bank.exoplayer_airplay_receiver.exoplayer2.customizations;
-
-public interface TextSynchronizer {
-  long getTextOffset();
-  void setTextOffset(long value);
-  void addTextOffset(long value);
-}
