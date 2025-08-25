7b5fa00665b7c4ca3eec46fc03154c2210ac33f3
helpgod11111 <helpgod11111@gmail.com>
2025-08-25 11:01:40 +0800

    Delete tests directory

--- tests/06. downloading to offline cache with adaptive manifest/1-no-encryption/.bin/2-serve/httpd.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-call "%~dp0..\..\..\0-tools\tools.bat"
-
-serve --cors --listen 80 --config "%~dpn0.json" "%~dp0..\.." >"%~dpn0.log" 2>&1
--- tests/06. downloading to offline cache with adaptive manifest/2-widevine/.bin/1-download-ts/dl.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-call "%~dp0..\..\..\0-tools\tools.bat"
-
-nget --insecure -P "%~dp0..\.." -i "%~dpn0.txt"
